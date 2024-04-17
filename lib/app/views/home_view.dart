a46e29e1faf957237c67c6740cd5bf029ee83c0b
Bruno Vinícius Alves Nogueira <100003070+vrunobinicius@users.noreply.github.com>
2024-04-17 16:25:03 -0300

    Small Adjustment
    AJUSTADO SCROLL DA INTERFACE

--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -22,55 +22,58 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Padding(
-          padding: EdgeInsets.only(left: 20, right: 20, top: 100, bottom: 100),
-          child: Column(
-            children: [
-              Image.asset(
-                'assets/logo.png',
-                width: 200,
-                height: 200,
-              ),
-              SizedBox(height: 90),
-              CurrencyBox(
-                items: homeController?.currencies,
-                controller: toText,
-                selectedItem: homeController?.toCurrency,
-                onChanged: (model) {
-                  setState(() {
-                    if (homeController!.fromCurrency != model) {
-                      homeController!.toCurrency = model!;
-                    }
-                  });
-                },
-              ),
-              CurrencyBox(
-                items: homeController?.currencies,
-                controller: fromText,
-                selectedItem: homeController?.fromCurrency,
-                onChanged: (model) {
-                  setState(() {
-                    if (homeController!.toCurrency != model) {
-                      homeController!.fromCurrency = model!;
-                    }
-                  });
-                },
-              ),
-              SizedBox(height: 50),
-              ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.amber),
-                      foregroundColor:
-                          MaterialStateProperty.all<Color>(Colors.black)),
-                  onPressed: () {
-                    homeController!.convert();
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding:
+                EdgeInsets.only(left: 20, right: 20, top: 100, bottom: 100),
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/logo.png',
+                  width: 200,
+                  height: 200,
+                ),
+                SizedBox(height: 90),
+                CurrencyBox(
+                  items: homeController?.currencies,
+                  controller: toText,
+                  selectedItem: homeController?.toCurrency,
+                  onChanged: (model) {
+                    setState(() {
+                      if (homeController!.fromCurrency != model) {
+                        homeController!.toCurrency = model!;
+                      }
+                    });
                   },
-                  child: Text("CONVERTER"))
-            ],
+                ),
+                CurrencyBox(
+                  items: homeController?.currencies,
+                  controller: fromText,
+                  selectedItem: homeController?.fromCurrency,
+                  onChanged: (model) {
+                    setState(() {
+                      if (homeController!.toCurrency != model) {
+                        homeController!.fromCurrency = model!;
+                      }
+                    });
+                  },
+                ),
+                SizedBox(height: 50),
+                ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.amber),
+                        foregroundColor:
+                            MaterialStateProperty.all<Color>(Colors.black)),
+                    onPressed: () {
+                      homeController!.convert();
+                    },
+                    child: Text("CONVERTER"))
+              ],
+            ),
           ),
         ),
       ),
